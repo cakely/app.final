854ef587bfae8340d323732aa91a28ea146f5b11
Steve Winton <stevewinton@gmail.com>
2020-05-26 11:37:55 -0500

    Open Source @cakely/app.final

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "domain_example" {
+}
+
+variable "cloudflare_email" {
+}
+
+variable "cloudflare_api_token" {
+}
+
+variable "cloudflare_zone_id" {
+}
+
+variable "github_verification_code" {
+}
